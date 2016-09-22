a7e54f6dd9866ee19b28515141b2901932b00446
temp <temp>
2016-09-22 11:26:27 -0400

    Add project files.

---
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.BackColor = System.Drawing.Color.Cornsilk;
+            this.openButton.Font = new System.Drawing.Font("Sitka Small", 15.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.openButton.ForeColor = System.Drawing.Color.DarkRed;
+            this.openButton.Location = new System.Drawing.Point(58, 12);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(152, 111);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open Greeting Card\r\n";
+            this.openButton.UseVisualStyleBackColor = false;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+    }
+}
+
---
+++ WindowsFormsApplication4/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            openButton.Hide();
+
+            
+            Graphics formGraphics = this.CreateGraphics();
+            formGraphics.Clear(Color.Black);
+            Pen drawPen = new Pen(Color.White, 5);
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            drawBrush.Color = Color.White;
+            Font drawFont = new Font("New Times Roman", 12, FontStyle.Italic);
+            formGraphics.DrawString("Virgo Zodiac", drawFont, drawBrush, 30, 200);
+
+            //Stars
+            formGraphics.FillEllipse(drawBrush, 40, 50, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 50, 50, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 8, 100, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 20, 25, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 90, 15, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 90, 90, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 75, 60, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 50, 90, 10, 10);
+            
+
+
+
+
+
+        }
+    }
+}
