d92341801aa75852224943943261eddc0fc3e1ae
temp <temp>
2016-09-27 13:35:52 -0400

    Started Connecting Dots with lines and changed color of stars

--- WindowsFormsApplication4/Form1.cs
+++ WindowsFormsApplication4/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace WindowsFormsApplication4
 {
@@ -41,11 +42,55 @@ namespace WindowsFormsApplication4
             formGraphics.FillEllipse(drawBrush, 230, 130, 10, 10);
             formGraphics.FillEllipse(drawBrush, 290, 70, 10, 10);
 
-            //connecting dots
-            formGraphics.DrawLine(drawPen, 30, 100, 100, 120);
-            Pen drawStar = new Pen(drawStar, Color, Yellow, 5);
-            formGraphics.FillEllipse(drawStar
-                )
+            Thread.Sleep(1000);
+           
+            //connecting dots and change color of dots
+            formGraphics.DrawLine(drawPen, 32, 102, 102, 122);
+            drawBrush.Color = Color.Yellow;
+            formGraphics.FillEllipse(drawBrush, 30, 100, 10, 10);
+            formGraphics.FillEllipse(drawBrush, 100, 120, 10, 10);
+
+            Thread.Sleep(1000);
+
+            //Connecting Dots and Change color
+            formGraphics.DrawLine(drawPen, 106, 123, 162, 172);
+            formGraphics.FillEllipse(drawBrush, 160, 170, 10, 10);
+
+            Thread.Sleep(1000);
+
+            //Connecting Dots and Change color
+           formGraphics.DrawLine(drawPen, 162, 172, 125, 230);
+           formGraphics.FillEllipse(drawBrush, 120, 230, 10, 10);
+
+            Thread.Sleep(1000);
+           
+            //Connecting Dots and Change color
+            formGraphics.DrawLine(drawPen, 162, 173, 234, 137);
+            formGraphics.FillEllipse(drawBrush, 230, 130, 10, 10);
+
+            Thread.Sleep(1000);
+
+            formGraphics.DrawLine(drawPen, 232, 131, 206, 78);
+            formGraphics.FillEllipse(drawBrush, 205, 75, 10, 10);
+
+            Thread.Sleep(1000);
+
+            formGraphics.DrawLine(drawPen, 205, 76, 192, 6);
+            formGraphics.FillEllipse(drawBrush, 190, 5, 10, 10);
+
+            Thread.Sleep(1000);
+
+            
+
+
+
+
+
+
+
+
+
+
 
 
 
